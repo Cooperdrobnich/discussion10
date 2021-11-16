c48c06a58c7fede0a7e8a2e999f80d4b7d83a1bd
Cooperdrobnich <drobnich@umich.edu>
2021-11-15 22:57:54 -0500

    a

--- discussion11.py
+++ discussion11.py
@@ -39,13 +39,13 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
-
+    cur.execute('DROP TABLE IF EXISTS Patients')
+    cur.execute('CREATE TABLE Patients ("pet_id" INTEGER PRIMARY KEY, "name" TEXT, "species_id" INTEGER, "age" INTEGER, "cuteness" INTEGER, "aggressiveness" INTEGER)')
+    conn.commit()
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
-    
+    cur.execute('INSERT OR IGNORE INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES ( ?, ?, ?, ?, ?, ?)', (0, "Fluffle", 0, 3, 90, 100 ) )
+    conn.commit()
 
 # TASK 2
 # CODE TO ADD JSON TO THE TABLE
@@ -53,19 +53,37 @@ def add_fluffle(cur, conn):
 def add_pets_from_json(filename, cur, conn):
     
     # WE GAVE YOU THIS TO READ IN DATA
-    f = open(filename)
+    f = open(os.path.dirname(os.path.abspath(__file__)) + '/' + filename)
     file_data = f.read()
     f.close()
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    count = 1
+    for i in json_data:
+
+        pet_id = count
+        name = i["name"]
+        cur.execute('SELECT id from Species WHERE title = ?',(i['species'], ))
+        species = int(cur.fetchone()[0])
+        age = int(i["age"])
+        cuteness = int(i["cuteness"])
+        aggressiveness = int(i["aggressiveness"])
+        cur.execute('INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)', (pet_id, name, species, age, cuteness, aggressiveness))
+        count += 1
+    conn.commit()
+
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    tuple_lst = []
+    cur.execute("SELECT name FROM Patients WHERE aggressiveness <= ?", (aggressiveness,))
+    rows = cur.fetchall()
+    for row in rows:
+        tuple_lst.append(row[0])
+    return tuple_lst
 
 
 
